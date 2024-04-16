0700ab68b5a5b22e07eff05eebc3f906d8d7bb1c
sierra182 <sierra-echo@msn.com>
2024-04-16 09:45:43 +0200

    end mic 0

--- rank04/microshell_0.c
+++ rank04/microshell_0.c
@@ -6,7 +6,7 @@
 /*   By: seblin <seblin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 13:28:00 by seblin            #+#    #+#             */
-/*   Updated: 2024/04/15 20:37:50 by seblin           ###   ########.fr       */
+/*   Updated: 2024/04/16 09:43:45 by seblin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 #include <sys/wait.h>
 #include <string.h>
 
-
-#include <stdio.h>
 int	display_error(char *err)
 {
 	while (*err)
@@ -25,12 +23,11 @@ int	display_error(char *err)
 
 int set_pipe(int pipe_fd[], int i)
 {	
-	//printf("set\n");
 	if (dup2(pipe_fd[i], i) < 0
 		|| close(pipe_fd[0]) < 0
 		|| close(pipe_fd[1]) < 0)
 		return (display_error("error: fatal\n"));
-	//printf("set fin\n");
+	*pipe_fd = 0;
 	return (0);
 }
 
@@ -38,30 +35,18 @@ int	parent(pid_t pid, int pipe_fd[])
 {
 	int	status;
 	
-	//printf("avant wait\n");
 	waitpid(pid, &status, 0);
-	//printf("apres wait\n");
 	if (*pipe_fd && set_pipe(pipe_fd, 0))
 		return (1);
 	return (WIFEXITED(status) && WEXITSTATUS(status));
 }
 
 int child(char *cmd[], char *cmd_end[], char *envp[], int pipe_fd[])
-{	
-	//printf("0 avant exec cmd:-%s-\n", *cmd_end);	
+{		
 	*cmd_end = 0;
-	//printf("0 avant exec cmd:-%s-\n", *cmd_end);
-	//printf("0 avant exec cmd:-%s-\n", *cmd);
-	// int i = 0;
-	// while (cmd[i])
-	// 	printf("-%s-\n", cmd[i++]);
-	//printf("pipe -%d-\n", *pipe_fd);
 	if (*pipe_fd && set_pipe(pipe_fd, 1))
 		return (1);
 	execve(*cmd, cmd, envp);
-	//printf("avant exec cmd:-%s-\n", *cmd);
-	
-	//printf("apres exec\n");
 	return (display_error("error: cannot execute "),
 			display_error(*cmd), display_error("\n")); 
 }
@@ -79,12 +64,10 @@ int cd(char *cmd_start[], char *cmd_end[])
 char	**forward(char **cmd_start)
 {
 	while (*cmd_start && **cmd_start && strcmp(*cmd_start, ";") && strcmp(*cmd_start, "|"))
-	{
-		//printf("y\n");
-		cmd_start++;
-	}		
+		cmd_start++;			
 	return (cmd_start);
 }
+
 int	nurcery(char *argv[], char *envp[])
 {
 	pid_t	pid;
@@ -99,15 +82,13 @@ int	nurcery(char *argv[], char *envp[])
 		cmd_end = forward(cmd_start);
 		if (*cmd_end && !strcmp(*cmd_end, "|"))
 		{
-			//printf("HAY PIEPE\n");
 			if (pipe(pipe_fd) < 0)
 			{
 				state = display_error("error: fatal\n");
 				continue ;
 			}			
-		}
-	
-		if (strcmp(*cmd_start, "cd"))
+		}	
+		if (*cmd_start != *cmd_end && strcmp(*cmd_start, "cd"))
 		{			
 			pid = fork();
 			if (!pid)
@@ -115,12 +96,10 @@ int	nurcery(char *argv[], char *envp[])
 			else
 				state = parent(pid, pipe_fd);	
 		}
-		else
+		else if (*cmd_start != *cmd_end)
 			state = cd(cmd_start, cmd_end);
 		argv = cmd_end;
-		*pipe_fd = 0;
-		//printf("argv: -%s-\n", *argv);
-	}	
+	}
 	return (state);
 }
 
