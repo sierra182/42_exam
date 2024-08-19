b8dd93d35f410616700258f5b519c4567ef0ed29
Sebastien Vidot <svidot@c2r6p6.42perpignan.fr>
2024-08-19 16:30:53 +0200

    clean wrlock

--- rank05/cpp_module00/Warlock.hpp
+++ rank05/cpp_module00/Warlock.hpp
@@ -6,34 +6,30 @@
 /*   By: svidot <svidot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/19 13:46:51 by svidot            #+#    #+#             */
-/*   Updated: 2024/08/19 15:43:49 by svidot           ###   ########.fr       */
+/*   Updated: 2024/08/19 16:30:30 by svidot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef WARLOCK_HPP
-#define WARLOCK_HPP
+#pragma once
 #include <iostream>
 
 class Warlock
-{
-        
-        std::string name;
-        std::string title;
-        
-        Warlock( void );
-        Warlock( const Warlock & src );
-        Warlock & operator=( const Warlock & rhs );
+{        
+    std::string name;
+    std::string title;
+    
+    Warlock( void );
+    Warlock( const Warlock & src );
+    Warlock & operator=( const Warlock & rhs );
         
     public :
         
         Warlock( const std::string & name, const std::string & title );
+        virtual ~Warlock( void );  
+           
         const std::string & getName() const;
         const std::string & getTitle() const;
         
         void setTitle( const std::string & title );
-        void introduce() const;
-        
-        virtual ~Warlock( void );     
+        void introduce() const;        
 };
-
-#endif
